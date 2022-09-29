3485b64d48b12d2d3c20f03c57bdeacd092786d8
Aleksey <alekseyshaykin2311@gmail.com>
2022-09-29 13:52:26 +0700

    Add Example005 Condition If Else

---
+++ Example005_Condition_If_Else/Program.cs
@@ -0,0 +1,17 @@
+﻿// Especial greatings
+
+Console.WriteLine("Ведите Ваше имя");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "маша")
+{
+    Console.Write("Я рад приветстовать тебя, ");
+    Console.WriteLine(username);
+}
+              
+else
+{
+ Console.Write("Привет, ");
+  Console.WriteLine (username);
+}
+ 
